08166d6d8c2c1b7cc76d9b523f0755f3523848af
Zhiming Zhou <zzm.vmiis@gmail.com>
2021-01-18 20:27:35 +1100

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -18,16 +18,18 @@
             title:'My communal ordering records',
             url:$H+"/communal-ordering/record/data.html",
             Table:"communal-ordering",
-            
             form_module:"communal-ordering-form-self", 
-            approval_module:prefix+'communal-ordering-approval-form',
-            approval_table:"communal-ordering-approval",
-            process_module:prefix+'communal-ordering-process-form',
-            process_table:"communal-ordering-process",
+            description:"Communal ordering data",
+            self:1,
+            
+            lab_manager_approval_module:prefix+'communal-ordering-lab-manager-approval-form',
+            lab_manager_approval_table:"communal-ordering-lab-manager-approval",
+            supervisor_approval_module:prefix+'communal-ordering-supervisor-approval-form',
+            supervisor_approval_table:"communal-ordering-supervisor-approval",
+            finacial_approval_module:prefix+'communal-ordering-financial-approval-form',
+            financial_approval_table:"communal-ordering-financial-approval",
             receive_module:prefix+'communal-ordering-receive-form',
             receive_table:"communal-ordering-receive",
-            self:1,
-            description:"Communal ordering data"
         },
         "communal-ordering-form-self":{
             title:'Communal ordering form',
@@ -46,13 +48,16 @@
             url:$H+"/communal-ordering/record/data.html",
             Table:"communal-ordering",
             form_module:"communal-ordering-form", 
-            approval_module:prefix+'communal-ordering-approval-form',
-            approval_table:"communal-ordering-approval",
-            process_module:prefix+'communal-ordering-process-form',
-            process_table:"communal-ordering-process",
+            description:"Communal ordering data",
+            
+            lab_manager_approval_module:prefix+'communal-ordering-lab-manager-approval-form',
+            lab_manager_approval_table:"communal-ordering-lab-manager-approval",
+            supervisor_approval_module:prefix+'communal-ordering-supervisor-approval-form',
+            supervisor_approval_table:"communal-ordering-supervisor-approval",
+            finacial_approval_module:prefix+'communal-ordering-financial-approval-form',
+            financial_approval_table:"communal-ordering-financial-approval",
             receive_module:prefix+'communal-ordering-receive-form',
             receive_table:"communal-ordering-receive",
-            description:"Communal ordering data"
         },
         "communal-ordering-form":{
             title:'Communal ordering form',
@@ -60,6 +65,33 @@
             Table:"communal-ordering",
             description:"Communal ordering form",
         },
+        "communal-ordering-lab-manager-approval-form":{
+            title:"Communal ordering lab manager approval",
+            url:$H+"/communal-ordering/lab-manager-approval/form.html",
+            Table:"communal-ordering-lab-manager-approval",
+            parent_table:"communal-ordering",
+            description:"Communal ordering lab manager approval",
+        },
+        "communal-ordering-supervisor-approval-form":{
+            title:"Communal ordering supervisor approval",
+            url:$H+"/communal-ordering/supervisor-approval/form.html",
+            Table:"communal-ordering-supervisor-approval",
+            parent_table:"communal-ordering",
+            description:"Communal ordering supervisor approval",
+        },
+        "communal-ordering-financial-approval-form":{
+            title:"Communal ordering financial approval",
+            url:$H+"/communal-ordering/financial-approval/form.html",
+            Table:"communal-ordering-financial-approval",
+            parent_table:"communal-ordering",
+            description:"Communal ordering financial approval",
+        },
+
+        
+
+
+
+
         "communal-ordering-approval-form":{
             title:"Communal ordering approval",
             url:$H+"/communal-ordering/approval/form.html",
