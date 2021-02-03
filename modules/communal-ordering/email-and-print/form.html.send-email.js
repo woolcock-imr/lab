cc3b44116d205f0069ef6d0da756ac030070462e
Zhiming Zhou <zzm.vmiis@gmail.com>
2021-02-03 19:30:31 +1100

    update

--- modules/communal-ordering/email-and-print/form.html.send-email.js
+++ modules/communal-ordering/email-and-print/form.html.send-email.js
@@ -6,14 +6,14 @@ m.send_email=function(record,url,callback){
             ddd[a]=record.Data[a];
         }
         var bk="<p style='page-break-before: always'>&nbsp;</p>";
-        ddd['form']=bk+txt;
+        ddd['purchase_order']=bk+txt;
         open_model__ID();
         $('#msg__ID').text('Sending email ... please wait!');
         $vm.vmpost(url,ddd,function(code){
             close_model__ID();
             callback(code)
         });
-       console.log(txt);
+        //console.log(txt);
     })
 }
 //-----------------------------------------
